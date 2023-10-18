55c2e98a49938f85d738f4919a8b1eed7965b8f4
user140242 <108657671+user140242@users.noreply.github.com>
2023-10-18 15:46:57 +0200

    Update Demo_SieveWheelBitSegmented.cpp
    added comment

--- Demo_SieveWheelBitSegmented.cpp
+++ Demo_SieveWheelBitSegmented.cpp
@@ -1,5 +1,31 @@
+/*
+MIT License
+
+Copyright (c) 2023 user140242
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+///     The following code is just a demonstration to understand how the sieve works, it must be optimized.
+
 ///     This is a implementation of the bit wheel segmented sieve 
-///     multi-threaded with OpenMP (compile with -fopenmp option)
+///     multi-threaded with OpenMP (compile with -fopenmp flag)
 ///     with max base wheel size choice  30 , 210 , 2310 
 
 #include <iostream>
